6ef1a05cc9a470181a8e599c862964a82ce9ad77
soer1013 <soer1013@naver.com>
2025-09-25 17:59:00 +0900

    feat: 작업환경 설정, 공룡 AI 기초작업중

---
+++ Assets/02.Scripts/Dinosaur/FearSystem.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FearSystem : MonoBehaviour
+{
+    
+
+    
+
+}
