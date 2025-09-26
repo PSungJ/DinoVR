b19f83994e0e7d71c640086b50723448e9d8a0de
soer1013 <soer1013@naver.com>
2025-09-26 17:58:04 +0900

    feat: 스테고 사우르스, 랩터 기본 AI 로직 구현

---
+++ Assets/02.Scripts/Dinosaur/Dinos/DinoStaga.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoStaga : DinoBase
+{
+
+    public override void Idle()
+    {
+        base.Idle();
+    }
+
+    public override void Roam()
+    {
+        base.Roam();
+    }
+
+    public override void Eating()
+    {
+        base.Eating();
+    }
+
+    public override void Drink()
+    {
+        base.Drink();
+    }
+
+    public override void Sleeping()
+    {
+        base.Sleeping();
+    }
+
+    public override void Fleeing()
+    {
+        base.Fleeing();
+    }
+
+    public override void Searching()
+    {
+        base.Searching();
+    }
+
+    public override void Attack()
+    {
+        base.Attack();
+    }
+
+    public override void Roar()
+    {
+        base.Roar();
+    }
+
+    public override void Death()
+    {
+        base.Death();
+    }
+}
