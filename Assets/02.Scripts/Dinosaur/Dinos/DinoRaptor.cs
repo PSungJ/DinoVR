562d73ae06b3b9283d634353e0a144d68d710bdf
soer1013 <soer1013@naver.com>
2025-10-14 17:23:05 +0900

    feat: 공룡 이동 매커니즘 수정
    이제 회전하는 방향으로 앞으로 나아가며 이동

--- Assets/02.Scripts/Dinosaur/Dinos/DinoRaptor.cs
+++ Assets/02.Scripts/Dinosaur/Dinos/DinoRaptor.cs
@@ -8,8 +8,9 @@ public class DinoRaptor : DinoBase
 
     public override void Searching()
     {
-        agent.isStopped = true;
-        
+        if (agent.hasPath)
+            agent.ResetPath();
+
         if (status.fearCurrent == 0 && status.target != null) // 공포가 0 이라면 == 사냥
         {
             float dis = (status.target.position - transform.position).magnitude;
@@ -45,10 +46,58 @@ public class DinoRaptor : DinoBase
         }
     }
 
-    public override void Attack()
+    float lastAttackTime = 0f;
+
+    public override void Chasing()
     {
-        base.Attack();
+        if (status.target == null)
+        {
+            ChangeState(DinoState.IDLE);
+            return;
+        }
+        if (Time.time - lastAttackTime > 5f)        // 마지막 공격으로 부터 5초가 넘었다면 추격 후 공격
+        {
+            agent.destination = status.target.position;
+            agent.speed = status.runSpeed;
+            if (Vector3.Distance(status.target.position, transform.position) <= status.attackRange)
+            {
+                agent.destination = transform.position;
+                ChangeState(DinoState.ATTACKING);
+            }
+        }
+        else if (!agent.hasPath)                    // 공격 후 5초간 랜덤 좌표 배회
+        {
+            agent.destination = GetRandomPoint(transform.position,20f);
+            agent.speed = status.runSpeed;
+        }
+    }
+
+
+    public override void Attack()    // 공격
+    {
+        if (status.target == null)
+        {
+            ChangeState(DinoState.IDLE);
+            return;
+        }
+        else
+        {
+            RotateSmoothly(status.target.position - transform.position);
+        }
+
         animator.SetTrigger(_aniAttack);        // 공격 애니메이션 재생
+
+        if (!isAnimating)
+        {
+            if (status.fearOrigin != null && status.IsAfraid())   // 공포원인이 있고 공포에 도달했다면 도망
+                StartFleeing();
+            else if (status.target != null)                       // 그런거 없고 공격중인 타겟이 있다면
+            {
+                isAnimating = true;
+                lastAttackTime = Time.time;
+                ChangeState(DinoState.CHASING);
+            }
+        }
     }
 
     public override void Call()
@@ -63,6 +112,7 @@ public class DinoRaptor : DinoBase
         else if (isAnimating == false)
         {
             ChangeState(DinoState.CHASING);
+            isAnimating = true;
             animator.ResetTrigger(_aniCall);
         }
     }
