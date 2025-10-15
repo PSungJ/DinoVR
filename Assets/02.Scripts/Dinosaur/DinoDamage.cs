bcde69f1735fdbe15509e247398b4e60665df3de
soer1013 <soer1013@naver.com>
2025-10-15 17:58:25 +0900

    feat: 피격 구현 및 밥먹기 구현중

--- Assets/02.Scripts/Dinosaur/DinoDamage.cs
+++ Assets/02.Scripts/Dinosaur/DinoDamage.cs
@@ -7,20 +7,33 @@ public class DinoDamage : MonoBehaviour
     public List<Collider> colliders;
 
     DinoStatus status;
+    DinoBase dino;
 
     void Start()
     {
         status = GetComponent<DinoStatus>();
+        dino = GetComponent<DinoBase>();
         GetComponentsInChildren<Collider>(colliders);
         colliders.RemoveAt(0);
         foreach (Collider col in colliders)
         {
-            col.isTrigger = false;
+            if(col.GetComponent<DinoAttack>() == null)
+                col.isTrigger = false;
         }
     }
 
     public void Damage(Collider col, float damage)
     {
+        if (col.tag == "Head")
+            damage *= 1.5f;
+        status.hpCurrent -= damage;
+        dino.Hit();
         Debug.Log($"{col.name} ¿¡ ¸ÂÀ½");
+
+        foreach (Collider c in colliders)
+        {
+            if (c.GetComponent<DinoAttack>() == null)
+                c.enabled = false;
+        }
     }
 }
