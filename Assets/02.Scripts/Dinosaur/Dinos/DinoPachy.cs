5d11256b49729b348369a9a1552ff62947abe565
soer1013 <soer1013@naver.com>
2025-10-16 16:05:30 +0900

    feat: 공룡 이동 수정

---
+++ Assets/02.Scripts/Dinosaur/Dinos/DinoPachy.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoPachy : DinoBase
+{
+
+    public override void Idle()
+    {
+        base.Idle();
+    }
+
+    public override void Roam()
+    {
+        base.Roam();
+    }
+
+    public override void Eating()
+    {
+        base.Eating();
+    }
+
+    public override void Drink()
+    {
+        base.Drink();
+    }
+
+    public override void Sleeping()
+    {
+        base.Sleeping();
+    }
+
+    public override void Fleeing()
+    {
+        base.Fleeing();
+    }
+
+    public override void Searching()
+    {
+        base.Searching();
+    }
+
+    public override void Attack()
+    {
+        base.Attack();
+        agent.ResetPath();
+        animator.SetTrigger(_aniAttack);
+        if (status.fearOrigin != null)
+            RotateSmoothly(status.fearOrigin.position - transform.position);
+    }
+
+    public override void Roar()
+    {
+        base.Roar();
+    }
+
+    public override void Death()
+    {
+        base.Death();
+    }
+}
