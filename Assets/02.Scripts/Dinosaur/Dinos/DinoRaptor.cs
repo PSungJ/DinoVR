211d2e6eae4236be1f1244ae31c5da3802066e4f
soer1013 <soer1013@naver.com>
2025-10-21 15:06:41 +0900

    fix: 공룡 배고픔 관련 행동 최종정리

--- Assets/02.Scripts/Dinosaur/Dinos/DinoRaptor.cs
+++ Assets/02.Scripts/Dinosaur/Dinos/DinoRaptor.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Net;
+using System.Timers;
 using UnityEngine;
 
 public class DinoRaptor : DinoBase
 {
+
 
     public override void Searching()
     {
@@ -19,21 +21,26 @@ public class DinoRaptor : DinoBase
             }
         }
 
-        if (status.fearCurrent <= 0 && status.target != null) // 공포가 0 이고, 타겟이 존재하면
+        if (status.fearCurrent <= 0 && status.target != null) // 배고픈 상태에서 공포가 0 이고, 타겟이 존재하면
         {
             if (!IsLive(status.target)) // 타겟이 죽어있다면 리턴
             {
                 ResetTarget();
                 return;
             }
-            float dis = (status.target.position - transform.position).magnitude;
-            if (dis > status.detactRange / 2f)  // 멀리서 접근하는 걸 발견했다면 바라보기
+            RotateSmoothly(status.target.position - transform.position);
+            currentAttackTime += Time.deltaTime;
+            if (currentAttackTime >= toAttackTime)  // 공격 대기시간 대기
             {
-                RotateSmoothly(status.target.position - transform.position);
-                currentAttackTime += Time.deltaTime;
-                if (currentAttackTime >= toAttackTime)
+                currentAttackTime = 0f;
+                float dis = (status.target.position - transform.position).magnitude;
+                if(status.meat != null && dis > status.detactRange / 2) // 먹을게 있는데 멀리서 다가온다면
                 {
-                    currentAttackTime = 0f;
+                    if (Time.time - lastRoarTime >= 5f)
+                        ChangeState(DinoState.ROAR);
+                }
+                else
+                {
                     ChangeState(DinoState.CALL);
                     isAnimating = true;
                 }
@@ -47,12 +54,16 @@ public class DinoRaptor : DinoBase
                 RotateSmoothly(status.fearOrigin.position - transform.position);
                 if (status.IsAfraid())
                     StartFleeing();
-                else if (status.fearCurrent >= status.fearThreshold * 0.2f)
+                else if (status.fearCurrent >= status.fearThreshold * 0.2f && Time.time - lastRoarTime >= 5f)
                     ChangeState(DinoState.ROAR);
             }
-            else if (dis > status.attackRange)  // 거리가 가깝지만 공격사거리 밖이라면 포효로 경고하기
+            else if (dis > status.attackRange * 2f && Time.time - lastRoarTime >= 5f)  // 거리가 가깝지만 공격사거리 밖이라면 포효로 경고하기
             {
                 ChangeState(DinoState.ROAR);
+            }
+            else
+            {
+                ChangeState(DinoState.CALL);
             }
         }
         else if (status.fearCurrent <= 0)
