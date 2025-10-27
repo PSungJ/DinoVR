dbd2af83920017473cdb6765ba2ef2cdee32586c
soer1013 <soer1013@naver.com>
2025-10-27 13:03:18 +0900

    fix: 사소한 코드 수정

--- Assets/02.Scripts/Dinosaur/Dinos/DinoTRex.cs
+++ Assets/02.Scripts/Dinosaur/Dinos/DinoTRex.cs
@@ -120,7 +120,8 @@ public class DinoTrex : DinoBase
         foreach (Collider col in dinos)
         {
             if (col.gameObject == gameObject) continue; // 자기 자신 제외
-            if (col.TryGetComponent<DinoStatus>(out DinoStatus stat))
+            DinoStatus stat = col.GetComponent<DinoStatus>();
+            if (stat != null)
             {
                 if (stat.stats.threat < status.stats.threat)
                 {
